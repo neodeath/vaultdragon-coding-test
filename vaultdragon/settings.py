6b8c68290fd29fecc78200ea38bec6d41498e3f8
nickolas <nickolas@localhost.localdomain>
2016-11-27 22:09:04 +0800

    fix setting databases

--- vaultdragon/settings.py
+++ vaultdragon/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'vaultdragon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
